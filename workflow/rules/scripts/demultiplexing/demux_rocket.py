0bd7316ab955137cfce5b146c48ec5b85278d4b5
Jobbie <jobvriet@gmail.com>
2023-12-15 15:19:42 +0100

    Removed testing break

--- workflow/rules/scripts/demultiplexing/demux_rocket.py
+++ workflow/rules/scripts/demultiplexing/demux_rocket.py
@@ -495,7 +495,6 @@ def sciseq_sample_demultiplexing(log: logging.Logger, experiment_name: str, samp
 
         if qc["n_pairs"] % 1000000 == 0:
             log.info("Processed %d read-pairs (%d discarded)", qc["n_pairs"], qc["n_pairs_failure"])
-            break
 
         # endregion --------------------------------------------------------------------------------------------------------------------------------
 
